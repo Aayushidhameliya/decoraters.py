f7be01a1b82edb9c6e9c64ed33e445ca058d79cc
Aayushidhameliya <141724255+Aayushidhameliya@users.noreply.github.com>
2024-06-18 17:04:46 +0530

    initial commit

--- decorater3/main.py
+++ decorater3/main.py
@@ -1,1 +1,23 @@
+import time
 
+current_time = time.time()
+print(current_time)
+
+def speed_calc_decorator(function):
+    def wrapper_function():
+        start_time = time.time()
+        function()
+        end_time = time.time()
+        print(f"{function.__name__} run speed: {end_time_start_time}s")
+    return wrapper_function()
+
+def fast_function():
+    for i in range(1000000):
+        i * i
+
+def slow_function():
+    for i in range(10000000):
+        i * i
+
+fast_function()
+slow_function()
